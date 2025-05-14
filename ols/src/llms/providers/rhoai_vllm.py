3b368f086379e11b8dd645a421e0b2abdd27142a
Tami Takamiya <ttakamiy@redhat.com>
2025-05-14 16:38:26 -0400

    Reformat using black

--- ols/src/llms/providers/rhoai_vllm.py
+++ ols/src/llms/providers/rhoai_vllm.py
@@ -54,8 +54,15 @@ class RHOAIVLLM(LLMProvider):
 
 
 class ChatRHOAI(ChatOpenAI):
-    """ Workaround for the compatibility issue between max_tokens and max_completion_tokens """
-    def _get_request_payload(self, input_: LanguageModelInput, *, stop: Optional[List[str]] = None, **kwargs: Any) -> dict:
+    """Workaround for the compatibility issue between max_tokens and max_completion_tokens"""
+
+    def _get_request_payload(
+        self,
+        input_: LanguageModelInput,
+        *,
+        stop: Optional[List[str]] = None,
+        **kwargs: Any,
+    ) -> dict:
         payload = super()._get_request_payload(input_, stop=stop, **kwargs)
         if "max_completion_tokens" in payload:
             payload["max_tokens"] = payload.pop("max_completion_tokens")
